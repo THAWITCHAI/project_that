ddedb329119d2c552b935dbba2d734a07718b165
Thawitchai <124795506+THAWITCHAI@users.noreply.github.com>
2024-08-13 07:42:42 +0700

    k

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+"use client";
+import Image from "next/image";
+import React, { useState } from "react";
 
-type Props = {}
+type Props = {};
 
 export default function Home({}: Props) {
+  const [select, setSelete] = useState(0);
   return (
-    <div className='name'>Pae สุดหล่อ</div>
-  )
+    <div className="home">
+      <div className="box-home">
+        {select == 0 ? (
+          <div className="login">
+            <div className="logo-image">
+              <Image
+                src={"/people.png"}
+                width={128}
+                height={128}
+                alt="Loading"
+                className="Image"
+                placeholder="empty"
+              />
+            </div>
+            <form action="" method="post" className="form">
+              <div className="box-email">
+                <div className="input-email">
+                  <Image
+                    src={"/user.png"}
+                    width={24}
+                    height={24}
+                    alt="Loading"
+                  />
+                  <input type="text" placeholder="Email" />
+                </div>
+              </div>
+            </form>
+          </div>
+        ) : (
+          <div className="Register">
+            
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
