2612c565d21778a5dd9a0779724768df27309ee5
Thawitchai <124795506+THAWITCHAI@users.noreply.github.com>
2024-08-16 02:36:38 +0700

    tailwind

--- src/app/_components/Client/List/List.tsx
+++ src/app/_components/Client/List/List.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./stlye.css";
-import './reponsive.css'
+import "./reponsive.css";
 type Props = {};
 
 export default function List({}: Props) {
@@ -9,10 +9,68 @@ export default function List({}: Props) {
       <div className="list-contact">
         <div className="list-header">
           <div className="list-count">
-                <button className="btn">จำนวนทั้งหมด: 10</button>
+            <button className="btn">จำนวนทั้งหมด: 10</button>
           </div>
         </div>
-        <div className="list-table"></div>
+        <div className="list-table">
+          <div className="table">
+            <div className="relative overflow-x-auto">
+              <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                  <tr className="text-black">
+                    <th scope="col" className="px-6 py-3 text-black">
+                      Product name
+                    </th>
+                    <th scope="col" className="px-6 py-3 text-black">
+                      Color
+                    </th>
+                    <th scope="col" className="px-6 py-3 text-black">
+                      Category
+                    </th>
+                    <th scope="col" className="px-6 py-3 text-black">
+                      Price
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    >
+                      Apple MacBook Pro 17
+                    </th>
+                    <td className="px-6 py-4">Silver</td>
+                    <td className="px-6 py-4">Laptop</td>
+                    <td className="px-6 py-4">$2999</td>
+                  </tr>
+                  <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    >
+                      Microsoft Surface Pro
+                    </th>
+                    <td className="px-6 py-4">White</td>
+                    <td className="px-6 py-4">Laptop PC</td>
+                    <td className="px-6 py-4">$1999</td>
+                  </tr>
+                  <tr className="bg-white dark:bg-gray-800">
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    >
+                      Magic Mouse 2
+                    </th>
+                    <td className="px-6 py-4">Black</td>
+                    <td className="px-6 py-4">Accessories</td>
+                    <td className="px-6 py-4">$99</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
