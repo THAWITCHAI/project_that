82fd8e093f185011eeaea9ef1391f55814e3bb2e
Thawitchai <124795506+THAWITCHAI@users.noreply.github.com>
2024-08-16 03:09:00 +0700

    Ok

--- src/app/_components/Client/List/List.tsx
+++ src/app/_components/Client/List/List.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./stlye.css";
 import "./reponsive.css";
+import Image from "next/image";
 type Props = {};
 
 export default function List({}: Props) {
@@ -18,53 +19,74 @@ export default function List({}: Props) {
               <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                   <tr className="text-black">
-                    <th scope="col" className="px-6 py-3 text-black">
-                      Product name
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-black font-normal"
+                    >
+                      รหัสการจอง
                     </th>
-                    <th scope="col" className="px-6 py-3 text-black">
-                      Color
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-black font-normal"
+                    >
+                      ยี่ห้อรถ
                     </th>
-                    <th scope="col" className="px-6 py-3 text-black">
-                      Category
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-black font-normal"
+                    >
+                      รุ่นรถ
                     </th>
-                    <th scope="col" className="px-6 py-3 text-black">
-                      Price
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-black font-normal"
+                    >
+                      ชื่อผู้จอง
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-black font-normal"
+                    >
+                      วันที่รับรถ
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-black font-normal"
+                    >
+                      วันที่คืนรถ
                     </th>
                   </tr>
                 </thead>
                 <tbody>
                   <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                    <th
-                      scope="row"
-                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                    >
-                      Apple MacBook Pro 17
-                    </th>
-                    <td className="px-6 py-4">Silver</td>
-                    <td className="px-6 py-4">Laptop</td>
-                    <td className="px-6 py-4">$2999</td>
-                  </tr>
-                  <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                    <th
-                      scope="row"
-                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                    >
-                      Microsoft Surface Pro
-                    </th>
-                    <td className="px-6 py-4">White</td>
-                    <td className="px-6 py-4">Laptop PC</td>
-                    <td className="px-6 py-4">$1999</td>
-                  </tr>
-                  <tr className="bg-white dark:bg-gray-800">
-                    <th
-                      scope="row"
-                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                    >
-                      Magic Mouse 2
-                    </th>
-                    <td className="px-6 py-4">Black</td>
-                    <td className="px-6 py-4">Accessories</td>
-                    <td className="px-6 py-4">$99</td>
+                    <td className="px-6 py-4 text-black ">8452</td>
+                    <td className="px-6 py-4 text-black ">Nissan</td>
+                    <td className="px-6 py-4 text-black ">GTR-35</td>
+                    <td className="px-6 py-4 text-black ">
+                      นาย ธวิชชัย บุญส่ง
+                    </td>
+                    <td className="px-6 py-4 text-black ">12/06/2545</td>
+                    <td className="px-6 py-4 text-black ">12/07/2545</td>
+                    <td className="px-6 py-4 text-black action">
+                      <button className="detail">
+                        <Image
+                          src={"/info.png"}
+                          alt="logo"
+                          width={24}
+                          height={24}
+                          className="Image"
+                        />
+                      </button>
+                      <button className="button">
+                        <Image
+                          src={"/delete.png"}
+                          alt="logo"
+                          width={24}
+                          height={24}
+                          className="Image"
+                        />
+                      </button>
+                    </td>
                   </tr>
                 </tbody>
               </table>
