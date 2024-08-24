0ee9f3fa3d8f0a5a55bbcc2eb60251c22d656d9f
Thawitchai <124795506+THAWITCHAI@users.noreply.github.com>
2024-08-25 03:43:54 +0700

    90%

--- src/app/_components/Client/All-Car/All_Cars.tsx
+++ src/app/_components/Client/All-Car/All_Cars.tsx
@@ -1,41 +1,58 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./reponsive.css";
-import "./style.css";
+import "./hee.css";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 type Props = {};
 
 export default function All_Cars({}: Props) {
+  const [dataCar, setDataCar] = useState([]);
   const router = useRouter();
+
+  useEffect(() => {
+    fetch("/api/car")
+      .then((res) => res.json())
+      .then((res) => setDataCar(res));
+  }, []);
+
   return (
     <div className="booking">
-      <div className="card">
-        <div className="card-image">
-          <Image
-            src={"/car-1.webp"}
-            width={570 / 2}
-            height={220 / 2} //Image size Width:570px,Heigth:220px
-            alt="image"
-            className="Image"
-          />
-        </div>
-        <div className="card-contact">
-          <div className="name">Alphard</div>
-          <div className="seat">จำนวนที่นั่ง: 4</div>
-          <div className="price">2,540 ฿</div>
-        </div>
-        <div className="card-btn">
-          <button
-            className="btn btn-1"
-            onClick={() => router.push("/client/detail-car/154")}
-          >
-            <Link href="/client/detail-car/154">รายละเอียด</Link>
-          </button>
-          <button className="btn btn-2 ">กำลังใช้งาน</button>
-        </div>
-      </div>
+      {dataCar.map((item, index) => {
+        return (
+          <div key={index} className="card">
+            <div className="card-image overflow-hidden mb-2">
+              <Image
+                src={item["cpath"]}
+                width={570 / 2}
+                height={220 / 2} //Image size Width:570px,Heigth:220px
+                alt="image"
+                className="Image"
+              />
+            </div>
+            <div className="card-contact">
+              <div className="name">{item["cmodel"]}</div>
+              <div className="seat">จำนวนที่นั่ง: {item["cseat"]}</div>
+              <div className="price">{item["cprice"]} ฿</div>
+            </div>
+            <div className="card-btn">
+              <button
+                className="btn btn-1"
+                onClick={() => router.push("/client/detail-car/" + item["cid"])}
+              >
+                รายละเอียด
+              </button>
+              {item["sid"] == "1" && (
+                <button className=" btn-2 bg-green-500">{item["sname"]}</button>
+              )}
+              {item["sid"] == "2" && (
+                <button className="btn-2 ">{item["sname"]}</button>
+              )}
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
