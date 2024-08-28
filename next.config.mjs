4029f8579d406913bd57af7ad7a3744bfe20aedb
Thawitchai <124795506+THAWITCHAI@users.noreply.github.com>
2024-08-28 18:03:07 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode:false,
+    images:{
+        remotePatterns:[
+            {
+                protocol:'http',
+                hostname:'192.168.1.166'
+            }
+        ]
+    }
+};
 
 export default nextConfig;
